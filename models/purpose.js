310eaebb7a76200be9f058bd389e4d66668e8a34
sooriya <info.uptoz@gmail.com>
2023-04-10 19:54:17 +0530

    model changes

--- models/purpose.js
+++ models/purpose.js
@@ -2,15 +2,6 @@ const mongoose = require('mongoose');
 
 const PurposeSchema = new mongoose.Schema({
 
-    name: {
-        type: String,
-        required: [false, 'Please add a name']
-    },
-    user_id: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'user',
-        required: false
-    },
     doctor_id: {
         type: mongoose.Schema.ObjectId,
         ref: 'doctor'
@@ -29,16 +20,7 @@ const PurposeSchema = new mongoose.Schema({
     deleted: {
         type: Boolean,
         default: false,
-    },
-    from: {
-        type: Date,
-        required: true
-    },
-    to: {
-        type: Date,
-        required: false
     }
-
 
 }, { timestamps: true });
 
