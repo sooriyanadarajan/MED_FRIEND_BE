3c393db92001008e78de7e1fc60cba4d88f8f186
sooriya <info.uptoz@gmail.com>
2023-04-10 19:58:23 +0530

    method changes

--- routers/purpose.js
+++ routers/purpose.js
@@ -10,7 +10,7 @@ const router = express.Router()
 
 //Doctor API's
 router.post('/create',  asyncHandler(purposeController.createPurpose))
-router.post('/list',  asyncHandler(purposeController.listPurpose))
+router.get('/list',  asyncHandler(purposeController.listPurpose))
 router.patch('/update',  asyncHandler(purposeController.updatePurpose))
 router.delete('/delete/:id',  asyncHandler(purposeController.deletePurpose))
 router.get('/status/:id',  asyncHandler(purposeController.changePurposeStatus))
