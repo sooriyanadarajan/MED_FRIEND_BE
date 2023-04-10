560e0a84eeba270aa6c336469e2aab432c6f07ef
sooriya <info.uptoz@gmail.com>
2023-04-10 23:33:18 +0530

    upload file

--- index.js
+++ index.js
@@ -11,9 +11,31 @@ const appointmentRouter = require('./routers/appointment')
 const purposeController = require('./routers/purpose')
 const dashboardRouter = require('./routers/dashboard')
 var cookieParser = require('cookie-parser')
+const multer  = require('multer');
+const path = require('path')
 
 const app = express()
 app.use(express.json())
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 const port = process.env.PORT || 7795
 // app.use(cors())
@@ -36,6 +58,16 @@ app.use('/doctor', doctorRouter);
 app.use('/appointment', appointmentRouter)
 app.use('/purpose', purposeController)
 app.use(dashboardRouter)
+
+app.post('/upload', upload.single('image'), (req, res, next) => {
+    try {
+        return res.status(201).json({
+            message: 'File uploded successfully'
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 const server = app.listen(port, () => {
     console.log("MED_FRIEND Running on : localhost", process.env.PORT);
