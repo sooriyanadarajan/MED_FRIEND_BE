02c981ebbd452d40862b6d785a0a9c54bb82d3b9
sooriya <info.uptoz@gmail.com>
2023-04-10 23:08:28 +0530

    appointment method get

--- routers/appointment.js
+++ routers/appointment.js
@@ -10,7 +10,7 @@ const router = express.Router()
 
 //Doctor API's
 router.post('/create',  asyncHandler(appointmentController.createAppointment))
-router.post('/list',  asyncHandler(appointmentController.listAppointment))
+router.get('/list',  asyncHandler(appointmentController.listAppointment))
 router.patch('/update',  asyncHandler(appointmentController.updateAppointment))
 router.delete('/delete/:id',  asyncHandler(appointmentController.deleteAppointment))
 router.get('/status/:id',  asyncHandler(appointmentController.changeAppointmentStatus))
