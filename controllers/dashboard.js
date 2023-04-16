cefc6195ee4bfaf540e21224358d030906a0b62a
sooriya <info.uptoz@gmail.com>
2023-04-16 23:06:47 +0530

    appointment list api

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -18,7 +18,7 @@ class DashboardController {
 
     async sendMail(req, res) {
 
-        const to = "akhilreddytaniparthi@gmail.com" ||  req.body.email
+        const to = "akhilreddytaniparthi@gmail.com" || req.body.email
         const name = req.body.name
         const num = req.body.mobile
         const message = req.body.message
@@ -43,44 +43,7 @@ class DashboardController {
             console.log('Email Sent Successfully', 'Result_id:', result);
         })
     }
-    // async listAssets(req, res) {
-    //     const limit = parseInt(req.body.limit)
-    //     const page = parseInt(req.body.page)
-    //     const skip = (page - 1) * limit
-    //     let userids = [];
-    //             if(req.body.key){
-    //                 userids = await User.find({$or: 
-    //                 [{name: { $regex: new RegExp(req.body.key, 'i') }}]}).distinct('_id');
-    //             }
-    //     let category = [];
-    //             if(req.body.key){
-    //                 category = await Category.find({$or: 
-    //                 [{categoryname: { $regex: new RegExp(req.body.key, 'i') }}]}).distinct('_id');
-    //             }
-    //     const search = req.body.key
-    //         ? {
-    //             $or: [
-    //                 { categorys: { $in : category }},
-    //                 { user_id: { $in : userids }},                   
-    //                 { imageName: { $regex: new RegExp(req.body.key, 'i')}}, 
-    //                 { description: { $regex: new RegExp(req.body.key, 'i')}}
-    //             ],
-    //         deleted:false}
-    //         : { 
-    //             deleted:false
-    //         };
-    //     let list = await Asset.find(search).populate('collections').populate('categorys').populate('favorites').populate('user_id').sort({ createdAt: -1 }).skip(skip).limit(limit)
-    //     let count = await Asset.countDocuments(search)
-    //     let output = {
-    //         list, count
-    //     }
-    //     res.status(200).json({ success: true, message: 'Recently Created Asset Listed', data: output })
-    // }
 
-    // async getAllTopAsset(req, res) {
-    //     let list = await Asset.find({deleted:false}).populate('collections').populate('categorys').populate('favorites').sort({ "owners": -1 }).limit(10)
-    //     res.status(200).json({ success: true, message: 'All Assets Listed', data: list })
-    // }
 
 
 }
--- routers/appointment.js
+++ routers/appointment.js
@@ -1,6 +1,8 @@
 const express = require('express')
 // const adminAuth = require('../middleware/auth-admin')
 const asyncHandler = require('../middlewares/async')
+const auth = require('../middlewares/auth')
+
 
 const AppointmentController = require('../controllers/appointment')
 const appointmentController = new AppointmentController();
@@ -10,7 +12,7 @@ const router = express.Router()
 
 //Doctor API's
 router.post('/create',  asyncHandler(appointmentController.createAppointment))
-router.get('/list',  asyncHandler(appointmentController.listAppointment))
+router.get('/list',  auth,asyncHandler(appointmentController.listAppointment))
 router.patch('/update',  asyncHandler(appointmentController.updateAppointment))
 router.delete('/delete/:id',  asyncHandler(appointmentController.deleteAppointment))
 router.get('/status/:id',  asyncHandler(appointmentController.changeAppointmentStatus))
